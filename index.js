b634e5058696b7338e507ccbf40af0fae4efb1cf
TechBoxNorth <techboxnorth@gmail.com>
2024-02-09 14:02:44 +0100

    continued work with the details section

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 let characters = [];
+let currentCharacter = [
+    character = {
+        name: 'name',
+        mass: '00kg',
+        hairColor: 'bald',
+        skinColor: 'albino',
+        eyeColor: 'transparent',
+        birthYear: '00BBY',
+        gender: 'mixed'
+    },
+    planet = {
+        name: 'death star',
+        rotation: '3 minutes',
+        orbit: '5 days',
+        diameter: '151515km',
+        climate: 'polar',
+        gravity: '0.23 standard',
+        terrain: 'asphalt'
+    }
+];
+console.log(currentCharacter[0].hairColor);
 const selectionList = document.querySelector('.selection-list');
 const swapi = 'https://swapi.dev/api';
 async function fetchPeople(){
@@ -20,19 +41,31 @@ fetchPeople().then((data) => {
     addCharactersToDom();
 });
 
-selectionList.addEventListener('click',(e) => {
-
+const selection = document.querySelector('.selection');
+selection.addEventListener('click',(e) => {
+    
+    if(e.target.classList.contains('list-item')){
+        console.log(characters[e.target.listId].name);
+    }/*else if(e.target.classList.contains('left')){
+        console.log('clicked on left arrow');
+    }else if(e.target.classList.contains('right')){
+        console.log('clicked on right arrow');
+    }
+    */
 });
 
 function addCharactersToDom(){
     for(let i = 0; i < characters.length; i++) {
         let person = document.createElement('div');
         person.classList.add('list-item');
+        person.listId = i;
         person.innerText = characters[i].name;
         if(i % 2 === 0){
             person.style.backgroundColor = 'var(--even-character)';
+            person.style.color = 'black';
         } else {
             person.style.backgroundColor = 'var(--odd-character)';
+            person.style.color = 'white';
         }
         
         selectionList.appendChild(person);
