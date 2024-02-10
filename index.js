4a94a9c72d7d60c7e22aa3401afb37d5e71b8b7d
TechBoxNorth <techboxnorth@gmail.com>
2024-02-10 14:41:50 +0100

    fetching data when clicking on character in characters list now works

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const selectionList = document.querySelector('.selection-list');
 const swapi = 'https://swapi.dev/api';
+
 async function fetchPeople(filter){
     try {
         response = await fetch(swapi + filter);
@@ -11,6 +12,18 @@ async function fetchPeople(filter){
         console.log('Error: ', error);
     }
 }
+
+let planetApi = '';
+async function fetchPlanet(planetApi){
+    try {
+        response = await fetch(planetApi);
+        planet = await response.json();
+        return planet;
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+}
+
 let characters = [];
 fetchPeople('/people/').then((data) => {
     for(let i = 0; i < data.results.length; i++){
@@ -26,16 +39,7 @@ console.log(characters)
 const selection = document.querySelector('.selection');
 selection.addEventListener('click',(e) => {
     if(e.target.classList.contains('list-item')){
-        fetchPeople('/people/' + (e.target.listId + 1) + '/').then((data) => {
-            document.querySelector('.char-name').innerText = data.name;
-            document.querySelector('.char-height').innerText = 'Height: ' + data.height + 'cm';
-            document.querySelector('.char-mass').innerText = 'Mass: ' + data.mass + 'kg';
-            document.querySelector('.char-hair').innerText = 'Hair color: ' + data.hair_color;
-            document.querySelector('.char-skin').innerText = 'Skin color: ' + data.skin_color;
-            document.querySelector('.char-eyes').innerText = 'Eye color: ' + data.eye_color;
-            document.querySelector('.char-birth').innerText = 'Year of birth: ' + data.birth_year;
-            document.querySelector('.char-gender').innerText = 'Gender: ' + data.gender;
-        });
+        displayCharacterData(e);
     }/*else if(e.target.classList.contains('left')){
         console.log('clicked on left arrow');
     }else if(e.target.classList.contains('right')){
@@ -63,15 +67,33 @@ function addCharactersToDom(){
     }
 }
 
-// function for setting character data in current object
-function setCurrentCharacterData(i){
-    currentCharacter[0].name = characters[i].name;
-    currentCharacter[0].mass = characters[i].mass;
-    currentCharacter[0].hairColor = characters[i].hair_color;
-    currentCharacter[0].skinColor = characters[i].skin_color;
-    currentCharacter[0].eyeColor = characters[i].eye_color;
-    currentCharacter[0].birthYear = characters[i].birth_year;
-    currentCharacter[0].gender = characters[i].gender;
+// function for setting character data in dom
+function displayCharacterData(e){
+    fetchPeople('/people/' + (e.target.listId + 1) + '/').then((data) => {
+        document.querySelector('.char-name').innerText = data.name;
+        document.querySelector('.char-height').innerText = 'Height: ' + data.height + 'cm';
+        document.querySelector('.char-mass').innerText = 'Mass: ' + data.mass + 'kg';
+        document.querySelector('.char-hair').innerText = 'Hair color: ' + data.hair_color;
+        document.querySelector('.char-skin').innerText = 'Skin color: ' + data.skin_color;
+        document.querySelector('.char-eyes').innerText = 'Eye color: ' + data.eye_color;
+        document.querySelector('.char-birth').innerText = 'Year of birth: ' + data.birth_year;
+        document.querySelector('.char-gender').innerText = 'Gender: ' + data.gender;
+        planetApi = data.homeworld;
+        console.log(planetApi);
+        displayPlanetData(planetApi);
+    });
+}
 
+function displayPlanetData(planetApi){
+    fetchPlanet(planetApi).then((planet) => {
+        console.log(planet);
+        document.querySelector('.planet-name').innerText = planet.name;
+        document.querySelector('.planet-rotation').innerText = 'Rotation period: ' + planet.rotation_period + ' hours';
+        document.querySelector('.planet-orbit').innerText = 'Orbital period: ' + planet.orbital_period + ' days';
+        document.querySelector('.planet-diameter').innerText = 'Diameter: ' + planet.diameter + ' kilometers';
+        document.querySelector('.planet-climate').innerText = 'Climate: ' + planet.climate;
+        document.querySelector('.planet-gravity').innerText = 'Gravity: ' + planet.gravity;
+        document.querySelector('.planet-terrain').innerText = 'Terrain: ' + planet.terrain;
+    });
 }
 
